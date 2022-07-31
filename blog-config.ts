ca917defd6e809e19888ee849e1158a518d57b17
emewjin <emewjin@gmail.com>
2022-07-31 18:07:56 +0900

    description 수정

--- blog-config.ts
+++ blog-config.ts
@@ -1,6 +1,7 @@
 const config = {
   title: 'emewjin.log', // Blog name
-  description: 'Frontend Dev',
+  description:
+    'Frontend Developer\n 최근에는 비즈니스를 멈추지 않으면서 바퀴를 갈아끼우는 방법과 디자인 시스템에 관심을 갖고 있습니다.',
   author: 'emewjin', // Your name
   introduction: 'Product Engineer, Web Frontend Engineer',
   siteUrl: 'https://emewjin.github.io',
