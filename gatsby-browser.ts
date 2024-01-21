831f2289ba109f7107e9ae830130b15ab7d89118
emewjin <emewjin@gmail.com>
2024-01-21 17:15:18 +0900

    style: prism theme nord로 수정

--- gatsby-browser.ts
+++ gatsby-browser.ts
@@ -2,6 +2,7 @@
 import '~/fonts/Pretendard/pretendard.css';
 
 // Prismjs Theme
-import '~/prism/prism-dracula-dark.css';
+// import '~/prism/prism-dracula-dark.css';
+import '~/prism/prism-nord.css';
 
 import './assets/fontawesome/css/all.min.css';
