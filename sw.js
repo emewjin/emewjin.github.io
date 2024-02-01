7c9783729b8ba162d79934e56ef287a47ae965bb
emewjin <emewjin@users.noreply.github.com>
2024-02-01 23:06:19 +0000

    Automated deployment: Thu Feb  1 23:06:19 UTC 2024 7aabd5d9c9bb0d6de361f746a539bd418c2d1ce8

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-395bba4b58b90c1f15c1.js"
+    "url": "webpack-runtime-73cc9e1a25cdef81e375.js"
   },
   {
     "url": "framework-f5e476341f16cb7964b6.js"
@@ -40,7 +40,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "bec079c0b55dc03d519bc7f54a005d15"
+    "revision": "94bc1db6801b399c2e073c0c6876dfd2"
   },
   {
     "url": "manifest.webmanifest",
